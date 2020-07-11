7cd1349b4e2d54115367d96dfd2662dd8b79fd02
oleacapricorn <fomaolga@gmail.com>
2020-07-12 02:32:05 +0300

    added logo component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <Logo/>
         <h1>Daily Smarty</h1>
+
       </div>
     );
   }

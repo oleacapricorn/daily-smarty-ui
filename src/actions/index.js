95cd5742b3c8f8c5238fcbc8dcb98d669eaa8cb1
oleacapricorn <fomaolga@gmail.com>
2020-07-19 15:11:18 +0300

    added SET_RESULTS_POSTS reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,32 +1,31 @@
-import { SET_RECENT_POSTS } from './types';
+import { 
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
+} from './types';
 
 import axios from 'axios';
 
 export function fetchRecentPosts() {
     return function(dispatch) {
-        //perform our request in here.
-        console.log('hello');
         axios.get('https://api.dailysmarty.com/posts')
             .then(response => {
-                console.log(response.data.posts);
                 dispatch({
                     type: SET_RECENT_POSTS,
                     payload: response.data.posts
                 })
             })
     }
-} 
+}
 
-export function fetchPostsWithQuery() {
+export function fetchPostsWithQuery(query, callback) {
     return function(dispatch) {
-        //perform our request in here.
         axios.get(`https://api.dailysmarty.com/search?q=${query}`)
             .then(response => {
-                console.log(response.data.posts);
-                // dispatch({
-                //     type: SET_RECENT_POSTS,
-                //     payload: response.data.posts
-                // })
+                dispatch({
+                    type: SET_RESULTS_POSTS,
+                    payload: response.data.posts
+                })
+                if(callback) { callback() }
             })
     }
-} 
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,2 @@
 export const SET_RECENT_POSTS = 'SET_RECENT_POSTS'; 
+export const SET_RESULT_POSTS = 'SET_RESULT_POSTS'; 
